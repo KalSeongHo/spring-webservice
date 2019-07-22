52c95168c777bd85bf12b51877d30485d4685fdc
kalsh <48197161+KalSeongHo@users.noreply.github.com>
2019-07-23 01:52:22 +0900

    게시글 목록 출력 기능 구현

--- src/main/java/com/kalsh/webservice/service/PostsService.java
+++ src/main/java/com/kalsh/webservice/service/PostsService.java
@@ -1,10 +1,14 @@
 package com.kalsh.webservice.service;
 
 import com.kalsh.webservice.domain.posts.PostsRepository;
+import com.kalsh.webservice.dto.posts.PostsMainResponseDto;
 import com.kalsh.webservice.web.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -16,6 +20,12 @@ public class PostsService {
         return postsRepository.save(dto.toEntity()).getId();
 
         // 반환값 ; getId()
+    }
 
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/kalsh/webservice/web/WebController.java
+++ src/main/java/com/kalsh/webservice/web/WebController.java
@@ -1,7 +1,9 @@
 package com.kalsh.webservice.web;
 
+import com.kalsh.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,8 +14,17 @@ public class WebController {
     // "handlebars" 사용으로 컨트롤러에서 문자열 반환시,
     // 앞의 path 와 뒤의 확장자는 자동으로 지정됨.
     // ( prefix : src/main/resources/templates, suffix: .hbs )
+
+//    @GetMapping("/")
+//    public String main() {
+//        return "main";
+//    }
+
+    private PostsService postsService;
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 }
